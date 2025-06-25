b638e7b9b96d06224f59fa05fba059a6e0759f71
xiaoxiao <981672419@qq.com>
2025-06-25 10:36:16 +0800

    一个核桃开三次

--- src/controllers/api/getVoidProjectionRewardsController.ts
+++ src/controllers/api/getVoidProjectionRewardsController.ts
@@ -5,13 +5,33 @@ import { getAccountIdForRequest } from "@/src/services/loginService";
 import { IVoidTearParticipantInfo } from "@/src/types/requestTypes";
 import { RequestHandler } from "express";
 
+// export const getVoidProjectionRewardsController: RequestHandler = async (req, res) => {
+//     const accountId = await getAccountIdForRequest(req);
+//     const data = getJSONfromString<IVoidProjectionRewardRequest>(String(req.body));
+//
+//     if (data.ParticipantInfo.QualifiesForReward && !data.ParticipantInfo.HaveRewardResponse) {
+//         const inventory = await getInventory(accountId);
+//         await crackRelic(inventory, data.ParticipantInfo);
+//         await inventory.save();
+//     }
+//
+//     const response: IVoidProjectionRewardResponse = {
+//         CurrentWave: data.CurrentWave,
+//         ParticipantInfo: data.ParticipantInfo,
+//         DifficultyTier: data.DifficultyTier
+//     };
+//     res.json(response);
+// };
+
+//新方法，一次遗物获取三次奖励
 export const getVoidProjectionRewardsController: RequestHandler = async (req, res) => {
     const accountId = await getAccountIdForRequest(req);
     const data = getJSONfromString<IVoidProjectionRewardRequest>(String(req.body));
 
     if (data.ParticipantInfo.QualifiesForReward && !data.ParticipantInfo.HaveRewardResponse) {
         const inventory = await getInventory(accountId);
-        await crackRelic(inventory, data.ParticipantInfo);
+        // 添加 rewardCount 参数
+        await crackRelic(inventory, data.ParticipantInfo, {}, 3); // 传递空对象作为inventoryChanges
         await inventory.save();
     }
 
@@ -22,6 +42,20 @@ export const getVoidProjectionRewardsController: RequestHandler = async (req, re
     };
     res.json(response);
 };
+
+interface IVoidProjectionRewardRequest {
+    CurrentWave: number;
+    ParticipantInfo: IVoidTearParticipantInfo;
+    VoidTier: string;
+    DifficultyTier: number;
+    VoidProjectionRemovalHash: string;
+}
+
+interface IVoidProjectionRewardResponse {
+    CurrentWave: number;
+    ParticipantInfo: IVoidTearParticipantInfo;
+    DifficultyTier: number;
+}
 
 interface IVoidProjectionRewardRequest {
     CurrentWave: number;
